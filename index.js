4dce33cd1dc8ffdd23e1685d642dc701777729c9
Kevin Payravi <kevinpayravi@gmail.com>
2019-05-18 00:45:52 +0200

    Switch fix

--- index.js
+++ index.js
@@ -123,51 +123,69 @@ function processIcon(node, type) {
 		case "news":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Historical_Newspaper_-_The_Noun_Project.svg/17px-Historical_Newspaper_-_The_Noun_Project.svg.png");
 			textNode.setAttribute("alt", "News from a reputable news agency");
+			break;
 		case "community":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Community_Noun_project_2280.svg/17px-Community_Noun_project_2280.svg.png");
 			textNode.setAttribute("alt", "Community-created news");
+			break;
 		case "opinion":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/FAQ_icon_%28Noun_like%29.svg/17px-FAQ_icon_%28Noun_like%29.svg.png");
 			textNode.setAttribute("alt", "Opinion");
+			break;
 		case "blog":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Feed_Noun_project_104.svg/17px-Feed_Noun_project_104.svg.png");
 			textNode.setAttribute("alt", "Blog");
+			break;
 		case "government":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Eye_-_The_Noun_Project.svg/17px-Eye_-_The_Noun_Project.svg.png");
 			textNode.setAttribute("alt", "State-controlled or state-associated media");
+			break;
 		case "tabloid":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Talking_%2849969%29_-_The_Noun_Project.svg/17px-Talking_%2849969%29_-_The_Noun_Project.svg.png");
 			textNode.setAttribute("alt", "Tabloid");
+			break;
 		case "press":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Noun_project_401.svg/17px-Noun_project_401.svg.png");
 			textNode.setAttribute("alt", "Press release");
+			break;
 		case "social":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Social_Media_-_The_Noun_Project.svg/17px-Social_Media_-_The_Noun_Project.svg.png");
 			textNode.setAttribute("alt", "Social media");
+			break;
 		case "book":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Education_-_The_Noun_Project.svg/17px-Education_-_The_Noun_Project.svg.png");
 			textNode.setAttribute("alt", "Published book");
+			break;
 		case "left":
 			textNode.setAttribute("src", "https://imgur.com/z5EtyJ8.png");
 			textNode.setAttribute("alt", "Identified as having a left bias by Media Bias/Fact Check");
+			break;
 		case "leftcenter":
 			textNode.setAttribute("src", "https://imgur.com/6Tpc9Ef.png");
 			textNode.setAttribute("alt", "Identified as having a center-left bias by Media Bias/Fact Check");
+			break;
 		case "center":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/LACMTA_Circle_Purple_Line.svg/17px-LACMTA_Circle_Purple_Line.svg.png");
 			textNode.setAttribute("alt", "Identified as having minimal bias by Media Bias Fact Check");
+			break;
 		case "rightcenter":
 			textNode.setAttribute("src", "https://imgur.com/EAOZ0f5.png");
 			textNode.setAttribute("alt", "Identified as having a center-right bias by Media Bias/Fact Check");
+			break;
 		case "right":
 			textNode.setAttribute("src", "https://imgur.com/KJRDo4u.png");
 			textNode.setAttribute("alt", "Identified as having a right bias by Media Bias/Fact Check");
+			break;
 		case "conspiracy":
 			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Noun_project_exclamation_icon_620383.svg/17px-Noun_project_exclamation_icon_620383.svg.png");
 			textNode.setAttribute("alt", "Identified as having a conspiracy-pushing source by Media Bias Fact Check");
+			break;
 		case "fake":
-		textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Noun_project_exclamation_icon_620383.svg/17px-Noun_project_exclamation_icon_620383.svg.png");
-		textNode.setAttribute("alt", "Identified as having a fake-news source by Media Bias Fact Check");
+			textNode.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Noun_project_exclamation_icon_620383.svg/17px-Noun_project_exclamation_icon_620383.svg.png");
+			textNode.setAttribute("alt", "Identified as having a fake-news source by Media Bias Fact Check");
+			break;
+		default:
+			break;
 	}
 	textNode.style.paddingRight = "5px";
 	textNode.style.width = "17px";
